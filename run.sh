41963a8ca03a813d8a827b5f12c65762ac34674e
hawthorn <patrik.hagedorn@gmx.de>
2015-07-30 11:59:47 +0200

    Added -p flag to mkdir which complains if the directory already exists.

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 sed -i'' "s|{HOSTNAME}|${HOSTNAME}|" /etc/supervisor/supervisord.conf
 
-mkdir -v /var/log/supervisor/${HOSTNAME}
+mkdir -pv /var/log/supervisor/${HOSTNAME}
 
 exec supervisord -n -c /etc/supervisor/supervisord.conf
